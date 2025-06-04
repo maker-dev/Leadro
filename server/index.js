e944aec17de09b5b359bb340f73617753f71eb32
maker-dev <anasaizen9@gmail.com>
2025-06-04 16:21:13 +0100

    ADD express structure

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
